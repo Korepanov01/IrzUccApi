beeeb7cdb12dce423b6e260a4b152dece599a3cf
Korepanov Pavel <korepanovpa01@gmail.com>
2023-01-12 14:37:42 +0400

    Добавлен AppContext

---
+++ IrzUccApi/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IrzUccApi
+{
+    public class ApplicationContext: DbContext
+    {
+        const string CONNECTION_STRING = "Host=localhost;Port=5432;Database=usersdb;Username=postgres;Password=пароль_от_postgres";
+
+        public ApplicationContext()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(CONNECTION_STRING);
+        }
+    }
+}
