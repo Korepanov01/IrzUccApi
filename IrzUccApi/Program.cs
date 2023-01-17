0d6ade3d4158255fb95276e3523d273eb0fa918d
Korepanov Pavel <korepanovpa01@gmail.com>
2023-01-17 15:57:49 +0400

    Настроена валидация токена

--- IrzUccApi/Program.cs
+++ IrzUccApi/Program.cs
@@ -1,11 +1,31 @@
 using IrzUccApi;
+using IrzUccApi.Configurations;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddAuthentication("Bearer")
-    .AddJwtBearer();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtConfig:SecurityKey"] ?? throw new ArgumentNullException())),
+
+            ValidateIssuer = true,
+            ValidIssuer = builder.Configuration["JwtConfig:Issuer"],
+
+            ValidateAudience = true,
+            ValidAudience = builder.Configuration["JwtConfig:Audience"],
+
+            ValidateLifetime = true
+        };
+    });
 builder.Services.AddAuthorization();
 
 builder.Services.AddControllers();
