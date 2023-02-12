1b6abfbe0c0c03e7ba53e8392390630d0b1058f0
Korepanov Pavel <korepanovpa01@gmail.com>
2023-02-12 15:23:25 +0400

    Написан метод для получения картинки

---
+++ IrzUccApi/Controllers/Images/ImagesController.cs
@@ -0,0 +1,38 @@
+﻿using IrzUccApi.Models.Db;
+using IrzUccApi.Models.Dtos;
+using IrzUccApi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IrzUccApi.Controllers.Images
+{
+    [Route("api/images")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private readonly AppDbContext _dbContext;
+        private readonly UserManager<AppUser> _userManager;
+
+        public ImagesController(AppDbContext dbContext, UserManager<AppUser> userManager, EmailService emailService)
+        {
+            _dbContext = dbContext;
+            _userManager = userManager;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            var image = await _dbContext.Images.FirstOrDefaultAsync(i => i.Id== id);
+            if (image == null)
+                return NotFound();
+
+            return Ok(new ImageDto(
+                image.Id, 
+                image.Name, 
+                image.Extension, 
+                image.Data));
+        }
+    }
+}
---
+++ IrzUccApi/Models/Dtos/ImageDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IrzUccApi.Models.Dtos
+{
+    public class ImageDto
+    {
+        public ImageDto(
+            string id, 
+            string name, 
+            string extension, 
+            string data)
+        {
+            Id = id;
+            Name = name;
+            Extension = extension;
+            Data = data;
+        }
+
+        public string Id { get; }
+        public string Name { get; }
+        public string Extension { get; }
+        public string Data { get; }
+    }
+}
