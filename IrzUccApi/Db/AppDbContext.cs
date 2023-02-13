6454016dd9d322736ab383ca828b488114f6fd56
Korepanov Pavel <korepanovpa01@gmail.com>
2023-02-13 14:53:33 +0400

    Выделен класс для сида БД

--- IrzUccApi/Db/AppDbContext.cs
+++ IrzUccApi/Db/AppDbContext.cs
@@ -9,11 +9,6 @@ namespace IrzUccApi.Db
         IdentityUserClaim<string>, AppUserRole, IdentityUserLogin<string>,
         IdentityRoleClaim<string>, IdentityUserToken<string>>
     {
-        private const string SuperAdminEmail = "user@example.com";
-        private const string SuperAdminPassword = "string";
-        private const string SuperAdminName = "Главный";
-        private const string SuperAdminSurname = "Администратор";
-
         public DbSet<Image> Images { get; set; }
         public DbSet<Chat> Chats { get; set; }
         public DbSet<Comment> Comments { get; set; }
@@ -34,52 +29,7 @@ namespace IrzUccApi.Db
             base.OnModelCreating(builder);
 
             DbConfigurer.Configure(builder);
-            SeedData(builder);
-        }
-        private static void SeedData(ModelBuilder builder)
-        {
-            var appRoles = new[] {
-                new AppRole
-                {
-                    Name = Enums.RolesNames.SuperAdmin,
-                    NormalizedName = Enums.RolesNames.SuperAdmin.ToUpper()
-                },
-                new AppRole
-                {
-                    Name = Enums.RolesNames.Admin,
-                    NormalizedName = Enums.RolesNames.Admin.ToUpper()
-                },
-                new AppRole
-                {
-                    Name = Enums.RolesNames.Support,
-                    NormalizedName = Enums.RolesNames.Support.ToUpper()
-                },
-                new AppRole
-                {
-                    Name = Enums.RolesNames.CabinetsManager,
-                    NormalizedName = Enums.RolesNames.CabinetsManager.ToUpper()
-                }
-            };
-            builder.Entity<AppRole>().HasData(appRoles);
-
-            var superAdmin = new AppUser
-            {
-                FirstName = SuperAdminName,
-                Surname = SuperAdminSurname,
-                UserName = SuperAdminEmail,
-                NormalizedUserName = SuperAdminEmail.ToUpper(),
-                Email = SuperAdminEmail,
-                NormalizedEmail = SuperAdminEmail.ToUpper()
-            };
-            superAdmin.PasswordHash = new PasswordHasher<AppUser>().HashPassword(superAdmin, SuperAdminPassword);
-            builder.Entity<AppUser>().HasData(superAdmin);
-
-            var superAdminUserRoles = appRoles.Select(r => new AppUserRole
-            {
-                RoleId = r.Id,
-                UserId = superAdmin.Id
-            });
-            builder.Entity<AppUserRole>().HasData(superAdminUserRoles);
-        }
+            DbSeeder.SeedData(builder);
+        }       
     }
 }
---
+++ IrzUccApi/Db/DbSeeder.cs
@@ -0,0 +1,48 @@
+﻿using IrzUccApi.Enums;
+using IrzUccApi.Models.Db;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace IrzUccApi.Db
+{
+    static public class DbSeeder
+    {
+        static private readonly string _superAdminEmail = "user@example.com";
+        static private readonly string _superAdminPassword = "string";
+        static private readonly string _superAdminName = "Главный";
+        static private readonly string _superAdminSurname = "Администратор";
+        
+        static public void SeedData(ModelBuilder builder)
+        {
+            var appRoles = new List<AppRole>();
+            foreach (var role in new[] { RolesNames.Admin, RolesNames.SuperAdmin, RolesNames.Support, RolesNames.CabinetsManager })
+            {
+                appRoles.Add(new AppRole
+                {
+                    Name = role,
+                    NormalizedName = role.ToUpper()
+                });
+            }
+            builder.Entity<AppRole>().HasData(appRoles);
+
+            var superAdmin = new AppUser
+            {
+                FirstName = _superAdminName,
+                Surname = _superAdminSurname,
+                UserName = _superAdminEmail,
+                NormalizedUserName = _superAdminEmail.ToUpper(),
+                Email = _superAdminEmail,
+                NormalizedEmail = _superAdminEmail.ToUpper(),
+            };
+            superAdmin.PasswordHash = new PasswordHasher<AppUser>().HashPassword(superAdmin, _superAdminPassword);
+            builder.Entity<AppUser>().HasData(superAdmin);
+
+            var superAdminUserRoles = appRoles.Select(r => new AppUserRole
+            {
+                RoleId = r.Id,
+                UserId = superAdmin.Id
+            });
+            builder.Entity<AppUserRole>().HasData(superAdminUserRoles);
+        }
+    }
+}
