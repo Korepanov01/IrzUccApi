a570c759339304850ce6eaddfef856bc7a371aad
Korepanov Pavel <korepanovpa01@gmail.com>
2023-03-26 19:01:23 +0400

    Подробные ошибки

--- IrzUccApi/Enums/RequestErrorMessages.cs
+++ IrzUccApi/Enums/RequestErrorMessages.cs
@@ -2,8 +2,13 @@
 {
     static public class RequestErrorMessages
     {
+        static public readonly string AccountDeactivated = "Account is deactivated!";
         static public readonly string UserDoesntExistMessage = "User does not exist!";
         static public readonly string EmailAlreadyRegistered = "Email already used!";
+        static public readonly string WrongPassword = "Wrong password!";
+
+        static public readonly string WrongRefreshToken = "Wrong refresh token!";
+        static public readonly string WrongJwt = "Wrong JWT!";
 
         static public readonly string UserAlreadyOnPosition = "User already on position!";
         static public readonly string PositionDoesntExistMessage = "Position does not exist!";
